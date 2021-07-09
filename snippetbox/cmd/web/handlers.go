22f3ea744bf6e5b100ae0eff1798e0af06c2a02c
thaipd <thaipd@vivas.vn>
2021-07-09 10:26:22 +0700

    refactor

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -6,29 +6,32 @@ import (
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		//http.NotFound(w, r)
+		app.infoLog.Println("r.URL.Path=", r.URL.Path)
+		app.notFound(w)
 		return
 	}
 
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.infoLog.Printf("id=%s, err=%v", r.URL.Query().Get("id"), err)
+		app.notFound(w)
 		return
 	}
 
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", 405)
+		w.Header().Set("Allow", "POST")
+		app.clientError(w, http.StatusMethodNotAllowed) // Use the clientError()
 		return
 	}
 
--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -5,21 +5,81 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	addr := flag.String("addr", ":8080", "HTTP network address")
-	dsn := flag.String("dsn", "root:vivas@123 sslmode=disable", "Postgres Datasource Name")
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+// Define an application struct to hold the application-wide dependencies for tAnd then in the handlers.go file update your handler functions so that
+//they become methods against the application struct…
+// web application. For now we'll only include fields for the two custom logger
+// we'll add more to it as the build progresses.
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
 
-	log.Println("Starting server on :8080")
-	err := http.ListenAndServe(":8080", mux)
-	log.Fatal(err)
+func main() {
+	// Define a new command-line flag with the name 'addr', a default value of
+	// and some short help text explaining what the flag controls. The value of
+	// flag will be stored in the addr variable at runtime.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Importantly, we use the flag.Parse() function to parse the command-line
+	// This reads in the command-line flag value and assigns it to the addr
+	// variable. You need to call this *before* you use the addr variable
+	// otherwise it will always contain the default value of ":4000". If any error
+	// encountered during parsing the application will be terminated.
+	flag.Parse()
+
+	f, err := os.OpenFile("/tmp/info.log", os.O_RDWR|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	// Use log.New() to create a logger for writing information messages. This
+	// three parameters: the destination to write the logs to (os.Stdout), a st
+	// prefix for message (INFO followed by a tab), and flags to indicate what
+	// additional information to include (local date and time). Note that the fl
+	// are joined using the bitwise OR operator |.
+	infoLog := log.New(f, "INFO\t", log.Ldate|log.Ltime)
+
+	// Create a logger for writing error messages in the same way, but use stde
+	// the destination and use the log.Lshortfile flag to include the relevant
+	// file name and line number.
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Initialize a new instance of application containing the dependencies.I understand that this approach might feel a bit complicated and
+	//convoluted, especially when an alternative is to simply make the
+	//infoLog and errorLog loggers global variables. But stick with me. As the
+	//application grows, and our handlers start to need more dependencies,
+	//this pattern will begin to show its worth.
+	//Adding a Deliberate Error
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
+	//mux := http.NewServeMux()
+	//mux.HandleFunc("/", app.home)
+	//mux.HandleFunc("/snippet", app.showSnippet)
+	//mux.HandleFunc("/snippet/create", app.createSnippet)
+
+	// Initialize a new http.Server struct. We set the Addr and Handler fields
+	// that the server uses the same network address and routes as before, and
+	// the ErrorLog field so that the server now uses the custom errorLog logge
+	// the event of any problems.
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	// Call the ListenAndServe() method on our new http.Server struct.
+	err = srv.ListenAndServe()
+
+	errorLog.Fatal(err)
 }
 
 func openDB(dsn string) *sql.DB {
